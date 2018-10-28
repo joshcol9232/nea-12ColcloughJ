4ed70c2eef2bb425b235f7d2dd60b40c2c239460
Josh <12ColcloughJ@lytchett.org.uk>
2018-10-28 21:52:06 +0000

    Don't import as much in start.py

--- code/python-go/start.py
+++ code/python-go/start.py
@@ -1,6 +1,6 @@
-import sys, os
-print(str(os.path.dirname(os.path.realpath(__file__))+"/kivyStuff"))
-sys.path.insert(0, str(os.path.dirname(os.path.realpath(__file__))+"/kivyStuff"))
+from sys.path import insert
+from os.path import dirname, realpath
+insert(0, str(dirname(realpath(__file__))+"/kivyStuff"))
 import ui
 
 ui.runUI()
