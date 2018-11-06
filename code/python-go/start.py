a250f7938bb81f649b593450a2ba197977141d80
12ColcloughJ <12colcloughj@lytchett.org.uk>
2018-11-06 16:47:51 +0000

    start.py Was importing too much.

--- code/python-go/start.py
+++ code/python-go/start.py
@@ -1,6 +1,6 @@
-import sys
+from sys import path
 from os.path import dirname, realpath
-sys.path.insert(0, str(dirname(realpath(__file__))+"/kivyStuff"))
+path.insert(0, str(dirname(realpath(__file__))+"/kivyStuff"))
 import ui
 
 ui.runUI()
