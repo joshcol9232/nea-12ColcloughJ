506aa2a6abfdf5cfb3ebe14a58712891b47cd6c0
Josh <12ColcloughJ@lytchett.org.uk>
2018-11-21 21:58:07 +0000

    Still fixing...
    Realised I have to change 64-bit input of compression to little endian

--- code/python-go/blake.go
+++ code/python-go/blake.go
@@ -17,7 +17,7 @@ var k = [8]uint64 {0x6A09E667F3BCC908,
                    0x5BE0CD19137E2179}
 
 
-var sigma = [10][16]uint64 {{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
+var sigma = [12][16]uint64 {{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
                             {14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3},
                             {11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4},
                             {7, 9, 3, 1, 13, 12, 11, 14, 2, 6, 5, 10, 4, 0, 15, 8},
@@ -26,7 +26,9 @@ var sigma = [10][16]uint64 {{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 1
                             {12, 5, 1, 15, 14, 13, 4, 10, 0, 7, 6, 3, 9, 2, 8, 11},
                             {13, 11, 7, 14, 12, 1, 3, 9, 5, 0, 15, 4, 8, 6, 2, 10},
                             {6, 15, 14, 9, 11, 3, 0, 8, 12, 2, 13, 7, 1, 4, 10, 5},
-                            {10, 2, 8, 4, 7, 6, 1, 5, 15, 11, 9, 14, 3, 12, 13, 0}}
+                            {10, 2, 8, 4, 7, 6, 1, 5, 15, 11, 9, 14, 3, 12, 13, 0},
+                            {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
+                            {14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3}}
 
 // Research: https://tools.ietf.org/pdf/rfc7693.pdf
 
@@ -54,14 +56,12 @@ func mix(v [16]uint64, a, b, c, d int, x, y uint64) [16]uint64 {
   return v
 }
 
-func compress(h [8]uint64, block [128]byte, t int, lastBlock bool) [8]uint64 {  // Compressing function
-  fmt.Println(block, "block------------------------")
+func compress(h [8]uint64, block [16]uint64, t int, lastBlock bool) [8]uint64 {  // Compressing function
+  fmt.Println(m, "block------------------------")
   var v = [16]uint64{} // Current vector
   for i := 0; i < 8; i++ {
     v[i] = h[i]
-  }
-  for i := 8; i < 16; i++ {
-    v[i] = k[i-8]
+    v[i+8] = k[i]
   }
 
   v[12] = v[12] ^ uint64(math.Mod(float64(t), 18446744073709552000)) //  2 ^ 64 = 18446744073709552000
@@ -71,16 +71,13 @@ func compress(h [8]uint64, block [128]byte, t int, lastBlock bool) [8]uint64 {  
     v[14] = ^v[14] // NOT
   }
 
-  j := 0
-  var m [16]uint64
-  for k := 0; k < 16; k++ {
-    fmt.Println(j, "j")
-    m[k] = uint64(block[j]) | (uint64(block[j+1]) << 8) | (uint64(block[j+2]) << 16) | (uint64(block[j+3]) << 24) | (uint64(block[j+4]) << 32) | (uint64(block[j+5]) << 40) | (uint64(block[j+6]) << 48) | (uint64(block[j+7]) << 56)
-    j += 8
+  var m [16] uint64
+  for i := 0; i < 16; i++ {  // Get little-endian of block
+    m[i] = getLilEndian(block[i])
   }
 
   for i := 0; i < 12; i++ {
-    sigRow := sigma[int(math.Mod(float64(i), 10))]
+    sigRow := sigma[i]
     fmt.Printf("%xSIGMA ROW\n", sigRow)
     // Mix
     v = mix(v, 0, 4,  8, 12, m[sigRow[0]], m[sigRow[1]])
@@ -109,24 +106,25 @@ func compress(h [8]uint64, block [128]byte, t int, lastBlock bool) [8]uint64 {  
 // r = 12 rounds
 // 16 64-bit words per block.
 // 512 bit
-func blake2b(data []byte, hashL int) [8]uint64 {  // M is message, l is length of message, hashL is length of hash wanted.
+func blake2b(data [][16]uint64, hashL int) [8]uint64 {  // data is split into 16 64-bit words.
   h := k
 
   h[0] = h[0] ^ (0x01010000 ^ uint64(hashL)) // Not using a key
 
-  var block [128]byte
+  if len(data) > 1 {
+    for i := 0; i < len(data)-2; i++ {  // Do all blocks apart from last one.
+      h = compress(h, data[i], (i+1)*128, false)  //128 block bytes = 16 64-bit words.
+    }
+  }
 
-  copy(block[:], data)
-  fmt.Println(block, "block")
-
-  h = compress(h, block, 11, true)
+  h = compress(h, data[len(data)-1], 11, true)
 
   return h
 }
 
 
 func main() {
-  g := []byte("")
+  g := [][16]uint64{{0}}
 
   h := blake2b(g, 128)
   fmt.Printf("%x\nLAST", h)
