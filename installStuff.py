9dcf7871781d631ce389e59566d446ccefa45097
Josh <joshuacolclough2@googlemail.com>
2019-03-14 09:59:30 +0000

    Install kivy script working

---
+++ installStuff.py
@@ -0,0 +1,3 @@
+from pip._internal import main as pipmain
+
+pipmain(["install", "-r", "requirements.txt"])
--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name="FileMate",
-      version="1",
-      description=("FileMate"),
-      install_requires=["kivy"]
-)
-
